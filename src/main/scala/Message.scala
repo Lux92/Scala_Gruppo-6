aa22fb804dce1a833fc676c222105170f5d86aa9
lux <lucianolimina92@gmail.com>
2018-02-19 19:07:01 +0100

    Removed verifyMessage function

--- src/main/scala/Message.scala
+++ src/main/scala/Message.scala
@@ -40,29 +40,6 @@
       } else {None}
     }
 
-    def verifyMessage(): Boolean ={
-      val mail  = this.unapply(this.txt).get
-      val src = mail._1
-      val dst = mail._2
-      val s = mail._3
-      val t = mail._4
-
-
-      if(verifyEmail(src))
-        println("Email sorgente corretta")
-      if(verifyEmail(dst))
-        println("Email destinatario corretta")
-      if(s.length()> 64)
-        println("Oggetto Email troppo grande")
-      if(t.length() > 255)
-        println("Testo Email troppo grande")
-
-      if(verifyEmail(src) && verifyEmail(dst) && s.length() <= 64 && t.length() <= 255)
-        true
-      else
-        false
-    }
-
     def verifyEmail(id : String): Boolean = {
 
       """(\w+)@([\w\.]+)""".r.unapplySeq(id).isDefined
